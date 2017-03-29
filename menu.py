5b34e169e3a2de75e0642715ad444ea6b8b5f9ee
aixoft <mr.paul.besset@gmail.com>
2017-03-29 14:14:01 +0200

    Modifications on menu

--- menu.py
+++ menu.py
@@ -3,3 +3,12 @@
 print "###########################################################"
 print "            Choose one of the following options            "
 print "###########################################################"
+print " 1 - Autodetect                                            "
+print "###########################################################"
+menuOption=int(raw_input())
+if(menuOption == 1):
+	autodetect()
+
+def autodetect():
+
+def ucopiaOneClickButton():
